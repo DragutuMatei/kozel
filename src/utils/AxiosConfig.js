1e4ee193cb4f236322306029ac609e0bf4d81287
matei <mateidr7@gmail.com>
2023-10-17 23:40:56 +0300

    axios configuration

---
+++ src/utils/AxiosConfig.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const axios_config = axios.create({
+  baseURL: "http://localhost:8080",
+  withCredentials: true,
+  headers: {
+    "content-type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+  },
+});
+
+export default axios_config;
