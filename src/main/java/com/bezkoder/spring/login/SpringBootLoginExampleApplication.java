11aa0de413f3977f218f1cf71d6ad5440148c36d
matei <mateidr7@gmail.com>
2023-10-26 02:08:40 +0300

    gdfgd

--- src/main/java/com/bezkoder/spring/login/SpringBootLoginExampleApplication.java
+++ src/main/java/com/bezkoder/spring/login/SpringBootLoginExampleApplication.java
@@ -1,9 +1,12 @@
 package com.bezkoder.spring.login;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class SpringBootLoginExampleApplication {
 
 	public static void main(String[] args) {
