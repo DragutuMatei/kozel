dad772259a3793491e8a29d658951076b4cf3a80
matei <mateidr7@gmail.com>
2023-11-17 15:16:45 +0200

    asd

--- src/utils/AxiosConfig.js
+++ src/utils/AxiosConfig.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axios_config = axios.create({
   // baseURL: "https://fastlane-nfzl.onrender.com/api",
-  baseURL: "http://localhost:8080/api",
+  // baseURL: "http://localhost:8080/api",
+  baseURL: "http://172.86.76.130:8080",
   withCredentials: true,
   headers: {
     // "access-control-expose-headers": "Set-Cookie",
